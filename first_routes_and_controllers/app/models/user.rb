85dd1ebdb994e75a68c7ff7d09a0bbba4eb16ef9
lavparker <lavinia.parker@outlook.com>
2022-06-29 16:31:58 -0400

    final commit first routes

---
+++ first_routes_and_controllers/app/models/user.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email)
+#
+class User < ApplicationRecord
+    validates :name, :email, presence: true
+    validates :email, uniqueness: true
+end
--- first_routes_and_controllers/config/routes.rb
+++ first_routes_and_controllers/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #resources :users
-  get 'users', to: 'users#index', as: 'user'
-  get 'users/:id', to: 'users#show', as: 'user'
-  post 'users', to: 'users#create', as: 'user'
-  patch 'users/:id', to: 'users#update', as: 'user'
-  put 'users/:id', to: 'users#update', as: 'user'
+  get 'users', to: 'users#index'#, as: 'user'
+  get 'users/:id', to: 'users#show'#, as: 'user'
+  post 'users', to: 'users#create'#, as: 'user'
+  patch 'users/:id', to: 'users#update'#, as: 'user'
+  put 'users/:id', to: 'users#update'#, as: 'user'
+  delete 'users/:id', to: 'users#destroy'#, as: 'user'
 end
